712d0319b0da8381786eba1592cc2621fce97df3
Alex Kelso <ark594@nyu.edu>
2022-10-15 17:37:40 -0400

    lil fixes to asyncioTest

--- software/prototyping-other/asyncioTest.py
+++ software/prototyping-other/asyncioTest.py
@@ -1,24 +1,25 @@
 import asyncio
 
+def larger(a,b):
+    if a > b:
+        return a
+    else: 
+        return b
 
-def sum(a,b):
-    return a+b
-
-def product(a,b):
-    return a*b
+def average(a,b):
+    return (float) (a+b)/2
 
 # main async function 
 async def main():
     a = int(input("Enter a number: "))
     b = int(input("Enter another number: "))
 
-    print("Calculating sum...")
-    await asyncio.sleep(5)
-    print(a, "+", b, "=", sum(a,b))
+    print("Calculating larger value...")
+    await asyncio.sleep(2)
+    print(larger(a,b), "is larger.")
 
-    print("Calculating product...")
+    print("Calculating average...")
     await asyncio.sleep(5)
-    print(a, "*", b, "=", product(a,b))
-
+    print("The average of", a, "and", b, "is", average(a,b))
 
 asyncio.run(main())
