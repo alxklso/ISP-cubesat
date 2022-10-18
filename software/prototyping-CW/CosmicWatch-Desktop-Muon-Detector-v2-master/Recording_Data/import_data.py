376daf037da47b8ac07367fdf688f133d02496ec
Alex Kelso <ark594@nyu.edu>
2022-10-18 17:49:36 -0400

    adjusted code for Python 3.x syntax

--- software/prototyping-CW/CosmicWatch-Desktop-Muon-Detector-v2-master/Recording_Data/import_data.py
+++ software/prototyping-CW/CosmicWatch-Desktop-Muon-Detector-v2-master/Recording_Data/import_data.py
@@ -30,110 +30,110 @@ Based on http://fabacademy.org/archives/2015/doc/WebSocketConsole.html
 ''' 
 
 def print_help1():
-    print('\n===================== HELP =======================')
-    print('This code looks through the serial ports. ')
-    print('You can select multiple ports with by separating the port number with commas.')
-    print('You must select which port contains the Arduino.\n')
-    print('If you have problems, check the following:')
-    print('1. Is your Arduino connected to the serial USB port?\n')
-    print('2. Check that you have the correct drivers installed:\n')
-    print('\tMacOS: CH340g driver (try: https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver)')
-    print('\tWindows: no dirver needed')
-    print('\tLinux: no driver needed')
+	print('\n===================== HELP =======================')
+	print('This code looks through the serial ports. ')
+	print('You can select multiple ports with by separating the port number with commas.')
+	print('You must select which port contains the Arduino.\n')
+	print('If you have problems, check the following:')
+	print('1. Is your Arduino connected to the serial USB port?\n')
+	print('2. Check that you have the correct drivers installed:\n')
+	print('\tMacOS: CH340g driver (try: https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver)')
+	print('\tWindows: no dirver needed')
+	print('\tLinux: no driver needed')
 
 
 clients = [] ## list of clients connected
 queue = multiprocessing.Queue() #queue for events forwarded from the device
 
 class DataCollectionProcess(multiprocessing.Process):
-    def __init__(self, queue):
-        #multiprocessing.Process.__init__(self)
-        self.queue = queue
-        self.comport = serial.Serial(port_name_list[0]) # open the COM Port
-        self.comport.baudrate = 9600          # set Baud rate
-        self.comport.bytesize = 8             # Number of data bits = 8
-        self.comport.parity   = 'N'           # No parity
-        self.comport.stopbits = 1 
+	def __init__(self, queue):
+		#multiprocessing.Process.__init__(self)
+		self.queue = queue
+		self.comport = serial.Serial(port_name_list[0]) # open the COM Port
+		self.comport.baudrate = 9600          # set Baud rate
+		self.comport.bytesize = 8             # Number of data bits = 8
+		self.comport.parity   = 'N'           # No parity
+		self.comport.stopbits = 1 
 
-    def close(self):
-        self.comport.close()
-        
-    def nextTime(self, rate):
-        return -math.log(1.0 - random.random()) / rate
+	def close(self):
+		self.comport.close()
+		
+	def nextTime(self, rate):
+		return -math.log(1.0 - random.random()) / rate
 
 def RUN(bg):
-    print 'Running...'
-    while True:
-        data = bg.comport.readline()
-        bg.queue.put(str(datetime.now())+" "+data)
-    
+	print('Running...')
+	while True:
+		data = bg.comport.readline()
+		bg.queue.put(str(datetime.now())+" "+data)
+	
 class WSHandler(tornado.websocket.WebSocketHandler):
-    def __init__ (self, application, request, **kwargs):
-        super(WSHandler, self).__init__(application, request, **kwargs)
-        self.sending = False
+	def __init__ (self, application, request, **kwargs):
+		super(WSHandler, self).__init__(application, request, **kwargs)
+		self.sending = False
 
-    def open(self):
-        print 'New connection opened from ' + self.request.remote_ip
-        clients.append(self)
-        print '%d clients connected' % len(clients)
-      
-    def on_message(self, message):
-        print 'message received:  %s' % message
-        if message == 'StartData':
-            self.sending = True
-        if message == 'StopData':
-            self.sending = False
+	def open(self):
+		print('New connection opened from ' + self.request.remote_ip)
+		clients.append(self)
+		print('%d clients connected' % len(clients))
+	  
+	def on_message(self, message):
+		print('message received:  %s' % message)
+		if message == 'StartData':
+			self.sending = True
+		if message == 'StopData':
+			self.sending = False
  
-    def on_close(self):
-        self.sending = False
-        clients.remove(self)
-        print 'Connection closed from ' + self.request.remote_ip
-        print '%d clients connected' % len(clients)
+	def on_close(self):
+		self.sending = False
+		clients.remove(self)
+		print('Connection closed from ' + self.request.remote_ip)
+		print('%d clients connected' % len(clients))
  
-    def check_origin(self, origin):
-        return True
+	def check_origin(self, origin):
+		return True
 
 def checkQueue():
-    while not queue.empty():
-        message = queue.get()
-        ##sys.stdout.write('#')
-        for client in clients:
-            if client.sending:
-                client.write_message(message)
+	while not queue.empty():
+		message = queue.get()
+		##sys.stdout.write('#')
+		for client in clients:
+			if client.sending:
+				client.write_message(message)
  
 
 def signal_handler(signal, frame):
-        print('You pressed Ctrl+C!')
-        ComPort.close()     
-        file.close() 
-        sys.exit(0)
+		print('You pressed Ctrl+C!')
+		ComPort.close()     
+		file.close() 
+		sys.exit(0)
 def serial_ports():
-    """ Lists serial port names
+	""" Lists serial port names
 
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-        sys.exit(0)
-    result = []
-    for port in ports:
-        try: 
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
+		:raises EnvironmentError:
+			On unsupported or unknown platforms
+		:returns:
+			A list of the serial ports available on the system
+	"""
+	if sys.platform.startswith('win'):
+		ports = ['COM%s' % (i + 1) for i in range(256)]
+	elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+		# this excludes your current terminal "/dev/tty"
+		ports = glob.glob('/dev/tty[A-Za-z]*')
+	elif sys.platform.startswith('darwin'):
+		ports = glob.glob('/dev/tty.*')
+	else:
+		raise EnvironmentError('Unsupported platform')
+		sys.exit(0)
+	result = []
+	for port in ports:
+		try: 
+			s = serial.Serial(port)
+			s.close()
+			result.append(port)
+		except (OSError, serial.SerialException):
+			pass
+	return result
 
 #If the Arduino is not recognized by your MAC, make sure you have
 #   installed the drivers for the Arduino (CH340g driver). Windows and Linux don't need it.
@@ -151,8 +151,8 @@ print("[h] Help")
 mode = str(raw_input("\nSelected operation: "))
 
 if mode == 'h':
-    print_help1()
-    sys.exit()
+	print_help1()
+	sys.exit()
 
 else:
 	mode = int(mode)
@@ -166,7 +166,7 @@ port_list = serial_ports()
 
 print('Available serial ports:')
 for i in range(len(port_list)):
-    print('['+str(i+1)+'] ' + str(port_list[i]))
+	print('['+str(i+1)+'] ' + str(port_list[i]))
 print('[h] help\n')
 
 ArduinoPort = raw_input("Selected Arduino port: ")
@@ -188,8 +188,8 @@ for i in range(len(ArduinoPort)):
 	port_name_list.append(str(port_list[int(ArduinoPort[i])-1]))
 
 if ArduinoPort == 'h':
-    print_help1()
-    sys.exit()
+	print_help1()
+	sys.exit()
 
 print("The selected port(s) is(are): ")
 for i in range(nDetectors):	 
@@ -203,11 +203,12 @@ if mode == 1:
 	detector_name_list = []
 	
 	if fname == '':
-	    fname = cwd+"/CW_data.txt"
-
+		fname = cwd+"/CW_data.txt"
+	
 	print('Saving data to: '+fname)
 
 	ComPort_list = np.ones(nDetectors)
+
 	for i in range(nDetectors):
 		signal.signal(signal.SIGINT, signal_handler)
 		globals()['Det%s' % str(i)] = serial.Serial(str(port_name_list[i]))
@@ -327,22 +328,22 @@ if mode == 2:
 			data = ComPort.readline()    # Wait and read data 
 			#print(data)
 			if 'Done' in data:
-			    ComPort.close() 
-			    sys.exit() 
+				ComPort.close() 
+				sys.exit() 
 			elif 'opening:' in data:
-			    fname = dir_path + '/' + data.split(' ')[-1].split('.txt')[0]+'.txt'
-			    
-			    print("Saving to: "+ fname)
-			    file = open(fname, "w",0)
-			    counter = 0
+				fname = dir_path + '/' + data.split(' ')[-1].split('.txt')[0]+'.txt'
+				
+				print("Saving to: "+ fname)
+				file = open(fname, "w",0)
+				counter = 0
 
 
 			elif 'EOF' in data:
-			    file.close() 
+				file.close() 
 
 			else:
-			    file.write(data)
-			    counter +=1
+				file.write(data)
+				counter +=1
    
 	else:
 		print('--- Error ---')
@@ -353,67 +354,65 @@ if mode == 2:
 		sys.exit()
 
 if mode == 3:
-    print("\nAre you sure that you want to remove all files from SD card?")
-    ans = raw_input("Type y or n: ")
-    if ans == 'y' or ans == 'yes' or ans == 'Y' or ans == 'YES':
-        signal.signal(signal.SIGINT, signal_handler)
-        ComPort = serial.Serial(port_name_list[0]) # open the COM Port
-        ComPort.baudrate = 9600          # set Baud rate
-        ComPort.bytesize = 8             # Number of data bits = 8
-        ComPort.parity   = 'N'           # No parity
-        ComPort.stopbits = 1 
+	print("\nAre you sure that you want to remove all files from SD card?")
+	ans = raw_input("Type y or n: ")
+	if ans == 'y' or ans == 'yes' or ans == 'Y' or ans == 'YES':
+		signal.signal(signal.SIGINT, signal_handler)
+		ComPort = serial.Serial(port_name_list[0]) # open the COM Port
+		ComPort.baudrate = 9600          # set Baud rate
+		ComPort.bytesize = 8             # Number of data bits = 8
+		ComPort.parity   = 'N'           # No parity
+		ComPort.stopbits = 1 
 
-        if ComPort.readline().strip() == 'CosmicWatchDetector':
-            time.sleep(1)
-            ComPort.write("remove")   
-            while True:
-                data = ComPort.readline()    # Wait and read data 
-                print(data)
-                if data == 'Done...\r\n':
-                    print("Finished deleting files.")
-                    break
-            ComPort.close()     
-            sys.exit()
+		if ComPort.readline().strip() == 'CosmicWatchDetector':
+			time.sleep(1)
+			ComPort.write("remove")   
+			while True:
+				data = ComPort.readline()    # Wait and read data 
+				print(data)
+				if data == 'Done...\r\n':
+					print("Finished deleting files.")
+					break
+			ComPort.close()     
+			sys.exit()
 
-        else:
-            print('--- Error ---')
-            print('You are trying to remove files from the microSD card.')
-            print('Is there an SD card inserted?')
-            print('Do you have the correct code on the Arduino? SDCard.ino')
-            print('Exiting ...')
-            sys.exit()
+		else:
+			print('--- Error ---')
+			print('You are trying to remove files from the microSD card.')
+			print('Is there an SD card inserted?')
+			print('Do you have the correct code on the Arduino? SDCard.ino')
+			print('Exiting ...')
+			sys.exit()
 
-    else:
-        print("Exiting ...")
-        sys.exit()
+	else:
+		print("Exiting ...")
+		sys.exit()
 
 if mode == 4:
-    bg = DataCollectionProcess(queue) 
-    #bg.daemon = True
-    #bg.start()
-    thread.start_new_thread(RUN,(bg,)) 
-    #p=multiprocessing.Process(target=RUN)
-    #p.start()
-    #server stuff
-    application = tornado.web.Application(
-        handlers=[(r'/', WSHandler)]
-    )
-    http_server = tornado.httpserver.HTTPServer(application)
-    port = 9090
-    http_server.listen(port)
-    myIP = socket.gethostbyname(socket.gethostname())
-    print 'CosmicWatch detector server started at %s:%d' % (myIP, port)
-    print 'You can now connect to your device using http://cosmicwatch.lns.mit.edu/'
-    mainLoop = tornado.ioloop.IOLoop.instance()
-    #in the main loop fire queue check each 100ms
-    try:
-    	scheduler = tornado.ioloop.PeriodicCallback(checkQueue, 100, io_loop = mainLoop)
-    except:
-    	# io_loop arguement was removed in version 5.x of Tornado.
-    	scheduler = tornado.ioloop.PeriodicCallback(checkQueue, 100)
-    scheduler.start()
-    #start the loop
-    mainLoop.start()
-
-
-
+	bg = DataCollectionProcess(queue) 
+	#bg.daemon = True
+	#bg.start()
+	thread.start_new_thread(RUN,(bg,)) 
+	#p=multiprocessing.Process(target=RUN)
+	#p.start()
+	#server stuff
+	application = tornado.web.Application(
+		handlers=[(r'/', WSHandler)]
+	)
+	http_server = tornado.httpserver.HTTPServer(application)
+	port = 9090
+	http_server.listen(port)
+	myIP = socket.gethostbyname(socket.gethostname())
+	print('CosmicWatch detector server started at %s:%d' % (myIP, port))
+	print('You can now connect to your device using http://cosmicwatch.lns.mit.edu/')
+	mainLoop = tornado.ioloop.IOLoop.instance()
+	#in the main loop fire queue check each 100ms
+	try:
+		scheduler = tornado.ioloop.PeriodicCallback(checkQueue, 100, io_loop = mainLoop)
+	except:
+		# io_loop arguement was removed in version 5.x of Tornado.
+		scheduler = tornado.ioloop.PeriodicCallback(checkQueue, 100)
+		scheduler.start()
+		#start the loop
+		mainLoop.start()
+		
