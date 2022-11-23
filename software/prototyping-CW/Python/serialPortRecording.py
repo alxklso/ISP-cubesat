bc83a71077fbaf25c9cb99ed043d75bd2d0bcbb5
Alex Kelso <ark594@nyu.edu>
2022-11-23 14:34:02 -0500

    Updated CW script w file-writing and sample data file

--- software/prototyping-CW/Python/serialPortRecording.py
+++ software/prototyping-CW/Python/serialPortRecording.py
@@ -1,4 +1,5 @@
 import serial
+import time
 
 """
 This is a script that listens to the serial port and displays the 
@@ -7,17 +8,53 @@ the UART pins on the Arduino Nano board to the serial port on your
 computer.
 """
 
-# Select correct port 
-ser = serial.Serial("/dev/tty.usbserial-0001")
-ser.flushInput() # for separating data 
+def displayMenu():
+    """Ask the user if they would like to file or 
+    only display to console
+    """
+    print("\nCosmic Watch to Serial Port Script")
+    print("-"*40)
+    print("Please select an option below:")
+    print("1. Display in console")
+    print("2. Write to file")
 
-while True:
-    try:
-        ser_bytes = ser.readline()
-        print(ser_bytes.decode("utf-8"))
+# Driver code
+def main():
+    # Connect to serial port
+    ser = serial.Serial("/dev/tty.usbserial-0001")
 
-    except:
-        print("Keyboard Interrupt")
-        break
+    # Display menu and ask for user input
+    displayMenu()
+    selection = int(input("\nSelection: "))
+    print("\n") # Space
+
+    # Respond to user input
+    if (selection == 1):
+        # Display to console
+        print("Displaying to console...")
+        while True:
+            serBytesDecoded = ser.readline().decode("utf-8") # Read in and decode data
+            print(serBytesDecoded) 
+
+    elif selection == 2:
+        # Attempt to write to file
+        fileName = input("Enter name of file (saved as txt): ")
+        
+        try: 
+            print("Attempting to write to file...")
+            f = open(fileName + ".txt", "a") # Append mode 
+
+            while True:
+                serBytesDecoded = ser.readline().decode("utf-8") # Read in and decode data
+                print(serBytesDecoded) 
+                f.write(serBytesDecoded)
+        
+        # If file cannot be opened
+        except:
+            print("Exiting...")
 
 
+    # For separating data 
+    ser.flushInput() 
+
+main()
