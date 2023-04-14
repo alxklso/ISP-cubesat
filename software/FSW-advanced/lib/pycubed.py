370535aebcac4de7c8b1002931a81f3a0cb20c1e
Ephil012 <Etp5501@gmail.com>
2023-04-14 19:21:25 -0400

    Swapped i2c

--- software/FSW-advanced/lib/pycubed.py
+++ software/FSW-advanced/lib/pycubed.py
@@ -83,10 +83,10 @@ class Satellite:
         self._chrg.switch_to_input()
 
         # Define SPI, I2C, UART
-        self.i2c1 = busio.I2C(board.SCL,board.SDA)
+        # self.i2c1 = busio.I2C(board.SCL,board.SDA)
         self.spi = board.SPI()
         self.uart = busio.UART(board.TX,board.RX)
-        #self.i2c2 = busio.I2C(board.SCL2, board.SDA2)
+        self.i2c2 = busio.I2C(board.SCL2, board.SDA2)
 
         # Define filesystem stuff
         self.logfile = "/log.txt"
