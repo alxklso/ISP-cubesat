c0811c4ce4815c11d914d545bff12f869e6fec57
Ephil012 <Etp5501@gmail.com>
2023-04-19 13:05:01 -0400

    Set send data to false

--- software/prototyping/proto-pycubed/sensor.py
+++ software/prototyping/proto-pycubed/sensor.py
@@ -5,7 +5,7 @@ from os import stat
 import adafruit_veml7700
 
 data_file = None
-SEND_DATA = True
+SEND_DATA = False # Do not turn to true until antenna is connected
 data_file=cubesat.new_file('/data/cw',binary=True)
 sensor = adafruit_veml7700.VEML7700(cubesat.i2c2)
 
