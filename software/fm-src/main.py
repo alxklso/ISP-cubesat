d91f948e89ec74a3386c913b0d2bfb2bd94cb786
Ephil012 <Etp5501@gmail.com>
2023-04-19 18:03:54 -0400

    Moved launch instructions

--- software/fm-src/main.py
+++ software/fm-src/main.py
@@ -7,13 +7,15 @@ from lib.pycubed import cubesat
 FSW MAIN: Creates a queue of scheduled tasks that runs forever. Includes
 a first-time startup burn-wire section, as well as batt pack voltage check.
 """
+
+############# CONFIGURATION START ############# 
+
 """
 IMPORTANT: Make sure to set the configuration below to the following before launch
 cubesat.antenna_attached = True
 cubesat.benchtop_testing = False
 cubesat.burn_enabled = True
 """
-############# CONFIGURATION START ############# 
 
 cubesat.antenna_attached = False # IMPORTANT: Only set to true if antenna is attached
 cubesat.benchtop_testing = True # IMPORTANT: Set to False when dropping off for flight
