8592e365a722235fe062d693b7d71b81ee208ea1
Ephil012 <Etp5501@gmail.com>
2023-04-22 16:46:02 -0400

    Cleaned up radio definitions

--- software/prototyping/feather_radio/feather_radio.ino
+++ software/prototyping/feather_radio/feather_radio.ino
@@ -6,42 +6,6 @@
 #define RFM95_RST 4
 #define RFM95_INT 7
 
-#if defined(ESP8266)
-  /* for ESP w/featherwing */ 
-  #define RFM95_CS  2    // "E"
-  #define RFM95_RST 16   // "D"
-  #define RFM95_INT 15   // "B"
-
-#elif defined(ADAFRUIT_FEATHER_M0) || defined(ADAFRUIT_FEATHER_M0_EXPRESS) || defined(ARDUINO_SAMD_FEATHER_M0)
-  // Feather M0 w/Radio
-  #define RFM95_CS      8
-  #define RFM95_INT     3
-  #define RFM95_RST     4
-
-#elif defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2) || defined(ARDUINO_NRF52840_FEATHER) || defined(ARDUINO_NRF52840_FEATHER_SENSE)
-  #define RFM95_INT     9  // "A"
-  #define RFM95_CS      10  // "B"
-  #define RFM95_RST     11  // "C"
-  
-#elif defined(ESP32)  
-  /* ESP32 feather w/wing */
-  #define RFM95_RST     27   // "A"
-  #define RFM95_CS      33   // "B"
-  #define RFM95_INT     12   //  next to A
-
-#elif defined(ARDUINO_NRF52832_FEATHER)
-  /* nRF52832 feather w/wing */
-  #define RFM95_RST     7   // "A"
-  #define RFM95_CS      11   // "B"
-  #define RFM95_INT     31   // "C"
-  
-#elif defined(TEENSYDUINO)
-  /* Teensy 3.x w/wing */
-  #define RFM95_RST     9   // "A"
-  #define RFM95_CS      10   // "B"
-  #define RFM95_INT     4    // "C"
-#endif
-
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 433.7
 
