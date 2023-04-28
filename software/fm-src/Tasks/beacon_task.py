084c23902bc40e71287715262b38f2012f11a4c2
Ephil012 <Etp5501@gmail.com>
2023-04-27 20:49:27 -0700

    Cleaned up beacon

--- software/fm-src/Tasks/beacon_task.py
+++ software/fm-src/Tasks/beacon_task.py
@@ -1,8 +1,11 @@
+import os
+import time
 import cdh
+import microcontroller
 from Tasks.template_task import Task
 
 class task(Task):
-    priority = 1
+    priority = 2
     testing_frequency = 1/30 # once every 5 mins, for listening for cmds
     frequency = 1/(60*40) # once every 40 mins, for listening for cmds
     name = "beacon"
@@ -25,20 +28,25 @@ class task(Task):
         super().__init__(satellite)
         # set our radiohead node ID so we can get ACKs
         self.cubesat.radio1.node = 0xFA # our ID
-        self.cubesat.radio1.destination = 0xAB # target's ID
+        self.cubesat.radio1.destination = 0xFF # target's ID
 
     async def main_task(self):
         """
         TODO: If you've attached an antenna,
-        set the cubesat.antenna_attached variable to True in main.py
+        set the cubesat.antenna_attached variable to True in pycubed.py
         to actually send the beacon packet
         """
         if self.cubesat.antenna_attached:
             self.debug("Sending beacon")
-            self.cubesat.radio_send("Hello World!", destination = 0xFF, keep_listening = True)
+            self.cubesat.radio_send(
+                f"vlt: {self.cubesat.battery_voltage} temp: {microcontroller.cpu.temperature}",
+                destination = 0xFF)
+            
+            self.transmit_cw_data()
 
-            self.debug("Listening 10s for response (non-blocking)")
-            heard_something = await self.cubesat.radio1.await_rx(timeout = 10)
+            self.debug("Listening for response (non-blocking)")
+            self.cubesat.radio1.listen()
+            heard_something = await self.cubesat.radio1.await_rx(timeout = 2400)
 
             if heard_something:
                 # retrieve response but don't ACK back unless an antenna is attached
@@ -54,33 +62,30 @@ class task(Task):
                     See beep-sat guide for more details
                     """
                     if len(response) >= 6:
-                        if not self.cubesat.antenna_attached:
-                            self.debug("Antenna not attached. Skipping over-the-air command handling")
-                        else:
-                            if response[:4] == self.super_secret_code:
-                                cmd=bytes(response[4:6]) # [pass-code(4 bytes)] [cmd 2 bytes] [args]
-                                cmd_args=None
-                                if len(response) > 6:
-                                    self.debug("command with args", 2)
-                                    try:
-                                        cmd_args = response[6:] # arguments are everything after
-                                        self.debug(f"cmd args: {cmd_args}", 2)
-                                    except Exception as e:
-                                        self.debug(f"arg decoding error: {e}", 2)
-                                if cmd in cdh.commands:
-                                    try:
-                                        if cmd_args is None:
-                                            self.debug(f"running {cdh.commands[cmd]} (no args)")
-                                            self.cmd_dispatch[cdh.commands[cmd]](self)
-                                        else:
-                                            self.debug(f"running {cdh.commands[cmd]} (with args: {cmd_args})")
-                                            self.cmd_dispatch[cdh.commands[cmd]](self, cmd_args)
-                                    except Exception as e:
-                                        self.debug(f"something went wrong: {e}")
-                                        self.cubesat.radio_send(str(e).encode())
-                                else:
-                                    self.debug("invalid command!")
-                                    self.cubesat.radio_send(b"invalid cmd" + response[4:])
+                        if response[:4] == self.super_secret_code:
+                            cmd=bytes(response[4:6]) # [pass-code(4 bytes)] [cmd 2 bytes] [args]
+                            cmd_args=None
+                            if len(response) > 6:
+                                self.debug("command with args", 2)
+                                try:
+                                    cmd_args = response[6:] # arguments are everything after
+                                    self.debug(f"cmd args: {cmd_args}", 2)
+                                except Exception as e:
+                                    self.debug(f"arg decoding error: {e}", 2)
+                            if cmd in cdh.commands:
+                                try:
+                                    if cmd_args is None:
+                                        self.debug(f"running {cdh.commands[cmd]} (no args)")
+                                        self.cmd_dispatch[cdh.commands[cmd]](self)
+                                    else:
+                                        self.debug(f"running {cdh.commands[cmd]} (with args: {cmd_args})")
+                                        self.cmd_dispatch[cdh.commands[cmd]](self, cmd_args)
+                                except Exception as e:
+                                    self.debug(f"something went wrong: {e}")
+                                    self.cubesat.radio_send(str(e).encode())
+                            else:
+                                self.debug("invalid command!")
+                                self.cubesat.radio_send(b"invalid cmd" + response[4:])
             else:
                 self.debug("no messages")
                 self.cubesat.radio1.sleep()
@@ -90,5 +95,27 @@ class task(Task):
             print()
             self.debug("[WARNING]")
             self.debug("NOT sending beacon (unknown antenna state)", 2)
-            self.debug("If you've attached an antenna, edit '/Tasks/beacon_task.py' to actually beacon", 2)
+            self.debug("If you've attached an antenna, edit '/pycubed.py' to actually beacon", 2)
             print()
+    
+    def transmit_cw_data(self):
+        files = [f for f in os.listdir("/sd/cw") if os.path.isfile(os.path.join("/sd/cw", f))]
+        files.sort()
+
+        start_time = time.monotonic()
+        end_time = start_time + (60*10) # run for 10 mins at max
+
+        for file in files:
+            print(f"\nSend CW data file: {self.data_file}")
+            with open(file, "rb") as f:
+                chunk = f.read(32) # Each reading is 32 bytes when encoded
+                while chunk:
+                    # We could send bigger chunks, radio packet can take 252 bytes
+                    self.cubesat.radio_send(chunk)
+                    print(chunk)
+                    chunk = f.read(32)
+            # Move to read directory when we have sent it
+            os.rename(file, file.replace("cw", "cw_read"))
+            # If we time out let's stop reading
+            if time.monotonic() > end_time:
+                break
