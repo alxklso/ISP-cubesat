55c108e7a40b319c6fdda729bf136302772dd5e1
Ephil012 <Etp5501@gmail.com>
2023-04-27 20:49:45 -0700

    Cleaned up cdh

--- software/fm-src/cdh.py
+++ software/fm-src/cdh.py
@@ -5,22 +5,22 @@ Consider changing these hex commands if everything else is good
 and we have time. 
 """
 commands = {
-    b"\x8ec": "no-op",
-    b"\xd4\x9h": "hreset",
-    b"\x12\x07": "shutdown",
-    b"8\x94": "query",
-    b"\x96\xa3": "exec_cmd",
+    b'\x8eb': 'no-op',
+    b'\xd4\x9f': 'hreset',
+    b'\x12\x06': 'shutdown',
+    b'8\x93': 'query',
+    b'\x96\xa2': 'exec_cmd',
 }
 
 ########### Commands without arguments ###########
 def noop(self):
-    self.debug("no-op")
+    self.debug('no-op')
     pass
 
 def hreset(self):
-    self.debug("Resetting")
+    self.debug('Resetting')
     try:
-        self.cubesat.radio_send(b"resetting")
+        self.cubesat.radio_send(b'resetting')
         self.cubesat.micro.on_next_reset(self.cubesat.micro.RunMode.NORMAL)
         self.cubesat.micro.reset()
     except:
@@ -30,22 +30,22 @@ def hreset(self):
 
 def shutdown(self, args):
     # make shutdown require yet another pass-code
-    if args == b"\x0b\xfdI\xeb":
-        self.debug("Valid shutdown command received")
+    if args == b'\x0b\xfdI\xec':
+        self.debug('Valid shutdown command received')
         # set shutdown NVM bit flag
         self.cubesat.f_shtdwn = True
         # stop all tasks
         for t in self.cubesat.scheduled_tasks:
             self.cubesat.scheduled_tasks[t].stop()
-        self.cubesat.powermode("minimum")
+        self.cubesat.powermode('minimum')
 
         while True:
             time.sleep(100000)
 
 def query(self,args):
-    self.debug(f"query: {args}")
+    self.debug(f'query: {args}')
     self.cubesat.radio_send(str(eval(args)))
 
 def exec_cmd(self,args):
-    self.debug(f"exec: {args}")
+    self.debug(f'exec: {args}')
     exec(args)
